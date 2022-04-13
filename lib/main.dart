50a745b32d80acea99382edaff8b1f6103469ffd
Karan Solanki <karansolanki@orient.com>
2022-04-13 13:02:40 +0530

    phone langugage wise app language need to change

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_change_language/LacaleString.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'HomePage.dart';
 import 'package:get/get.dart';
 
+import 'local.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // @override
+  // void didChangeDependencies() {
+  //   getLocale().then((locale) {
+  //     setState(() {
+  //       print("Preference Revoked ${locale.languageCode}");
+  //       Get.updateLocale(locale);
+  //       // print("GET LOCALE Revoked ${Get.locale.languageCode}");
+  //     });
+  //   });
+  //   super.didChangeDependencies();
+  // }
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       translations: LocaleString(),
-      locale: Locale('en','US'),
+      locale: Get.deviceLocale,
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: HomePage(),
     );
   }
 }
-
-
-
-
-
